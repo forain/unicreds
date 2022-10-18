3faae089c0dc7ac322fe4ea38dba082c34fd307b
Leandro Forain <leandro.forain@instructure.com>
2022-10-18 12:29:20 +0000

    Forked from github.com/Versent/unicreds

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/apex/log/handlers/cli"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
-	"github.com/versent/unicreds"
+	"github.com/forain/unicreds"
 )
 
 func init() {
